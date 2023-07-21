59678f4045506061712188b357542d998c48d437
Nikita Tomilov <programmer174@icloud.com>
2023-07-21 16:44:53 +0200

    Add display backlight fade

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
 uint16_t colors[12] = {
-    0x0000, // чёрный
-    0xFFFF, // белый
-    0xBDF7, // светло серый
-    0x7BEF, // серый
-    0xF800, // красный
-    0xFFE0, // жёлтый
-    0xFBE0, // оранжевый
-    0x79E0, // коричневый
-    0x7E0,  // зелёный
-    0x07FF, // голубой
-    0x001F, // синий
-    0xF81F  // розовый
+    0x0000,
+    0xFFFF, 
+    0xBDF7, 
+    0x7BEF, 
+    0xF800, 
+    0xFFE0, 
+    0xFBE0, 
+    0x79E0, 
+    0x7E0,  
+    0x07FF, 
+    0x001F, 
+    0xF81F 
 };
 
 #include <Keyboard.h>
@@ -24,35 +24,13 @@ uint16_t colors[12] = {
 EncButton2<EB_ENCBTN> enc1(INPUT, 1, 0, 2); //A, B, button 
 EncButton2<EB_ENCBTN> enc2(INPUT, 14, 15, 13); //A, B, button 
 
-void print(String line) {
-  tft.setTextFont(4);
-  tft.setCursor(0, 0);
-  tft.println(line);
-}
+int backlightPin = 25;
+int backlightBrightness = 1024;
 
-void normalPress(uint8_t keycode) {
-  Keyboard.press(keycode);
-  delay(10);
-  Keyboard.release(keycode);
-}
+long lastKeyPressTimestamp = millis();
+String previousLine = "";
 
-void consumerPress(uint16_t keycode) {
-  Keyboard.consumerPress(keycode);
-  delay(10);
-  Keyboard.consumerRelease();
-}
-
-void setup(void) {
-  Serial.begin(115200);
-  Serial.println("Init screen");
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_WHITE);
-  Serial.println("Init screen done");
-
-  Keyboard.begin();
-  Serial.println("Init USB done");
-
+void resetScreen() {
   byte color = 0;
   for(byte y=0; y<2; y++){
       for(byte x=0; x<6; x++){
@@ -62,10 +40,69 @@ void setup(void) {
   }
 }
 
+void print(String line) {
+  if (line.compareTo(previousLine) != 0) {
+    resetScreen();
+    tft.setTextFont(4);
+    tft.setCursor(0, 0);
+    tft.println(line);
+  }
+  previousLine = line;
+}
+
+void keyPressDetected() {
+  lastKeyPressTimestamp = millis();
+}
+
+void normalPress(uint8_t keycode) {
+  keyPressDetected();
+  Keyboard.press(keycode);
+  delay(10);
+  Keyboard.release(keycode);
+}
+
+void consumerPress(uint16_t keycode) {
+  keyPressDetected();
+  Keyboard.consumerPress(keycode);
+  delay(10);
+  Keyboard.consumerRelease();
+}
+
+void setup(void) {
+  Serial.begin(115200);
+
+  Serial.println("Init screen");
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_WHITE);
+  print("Hello!");
+  Serial.println("Init screen done");
+
+  Keyboard.begin();
+  Serial.println("Init USB done");
+
+  analogWriteFreq(1000);
+  analogWriteRange(1024);
+  pinMode(backlightPin, OUTPUT);
+}
+
 void loop() {
   delay(1);
   enc1.tick();
   enc2.tick();
+
+  analogWrite(backlightPin, backlightBrightness);
+  if (millis() - lastKeyPressTimestamp < 3000) {
+    backlightBrightness = 1024;
+  } else {
+    if (backlightBrightness > 0) {
+      backlightBrightness -= 1;
+    }
+  }
+
+  if (enc1.press() || enc2.press()) {
+    keyPressDetected();
+  }
 
   if (enc1.click()) {
     print("Press 1");
